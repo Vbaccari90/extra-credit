965937131c6a37cdc6439caeba5dce8d3b32b14c
Vincent Baccari <baccarivincent@gmail.com>
2024-11-14 15:07:01 -0500

    added add_numbers function

--- math_ops.py
+++ math_ops.py
@@ -4,8 +4,14 @@ def divide_numbers(a, b):
     """Divides two numbers and returns the result."""
     return a / b  # Potential division by zero error
 
+def add_numbers (a, b):
+    """adds two numbers together"""
+    return a + b
+
 if __name__ == "__main__":
     x = 10
     y = 0
     result = divide_numbers(x, y)
+    result_add = add_numbers(x, y)
     print(f"The result of division is: {result}")
+    print(f"The results of the addition is: {result_add}")
